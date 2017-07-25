fe079355efd26190c5c90752019879a45a51c897
gelingyan <gely@jikon.cn>
2017-07-25 11:57:49 +0800

    歌手详情页完成

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
@@ -9,16 +9,23 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'api/song'
+  import MusicList from 'components/music-list/music-list'
   export default {
-    computed: {
-      ...mapGetters([
-        'singer'
-      ])
-    },
     data() {
       return {
         songs: []
       }
+    },
+    computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
+      ...mapGetters([
+        'singer'
+      ])
     },
     created() {
       this._getDetail()
@@ -47,21 +54,14 @@
         return ret
       }
     },
-    components: {}
+    components: {
+      MusicList
+    }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import '~common/stylus/variable'
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
