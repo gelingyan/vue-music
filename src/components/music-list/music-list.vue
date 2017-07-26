cfe8389fac0afecc207458d46a2fab679d331327
gelingyan <gely@jikon.cn>
2017-07-26 14:47:50 +0800

    播放器基础组件完成:player

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
@@ -16,7 +16,7 @@
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list @select="selectItem" :songs="songs"></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -30,6 +30,7 @@
   import SongList from 'base/song-list/song-list'
   import {prefixStyle} from 'common/js/dom'
   import Loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
@@ -72,7 +73,19 @@
     methods: {
       scroll(pos) {
         this.scrollY = pos.y
-      }
+      },
+      back() {
+        this.$router.back()
+      },
+      selectItem(item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      ...mapActions([
+        'selectPlay'
+      ])
     },
     watch: {
       scrollY(newY) {
--- src/store/state.js
+++ src/store/state.js
@@ -1,8 +1,16 @@
 /**
  * Created by gelingyan on 2017/7/24.
  */
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false, // 全屏
+  playlist: [], // 播放列表
+  sequenceList: [], // 顺序列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1 // 当前播放索引
 }
 
 export default state
