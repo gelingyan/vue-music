ef08e116751ff5e1597966fac5ea76ab23ca8891
gelingyan <gely@jikon.cn>
2017-07-31 20:04:29 +0800

    播放器--全屏：完成进度条交互

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -28,7 +28,9 @@
         <div class="bottom">
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
-            <div class="progress-bar-wrapper"></div>
+            <div class="progress-bar-wrapper">
+              <progress-bar :percent="percent" @percentChange="onProcessBarChange"></progress-bar>
+            </div>
             <span class="time time-r">{{format(currentSong.duration)}}</span>
           </div>
           <div class="operators">
@@ -76,6 +78,7 @@
   import {mapGetters, mapMutations} from 'vuex'
   import animations from 'create-keyframe-animation'
   import {prefixStyle} from 'common/js/dom'
+  import ProgressBar from 'base/progress-bar/progress-bar'
 
   const transform = prefixStyle('transform')
 
@@ -183,6 +186,12 @@
         const second = this._pad(interval % 60)
         return `${minute}:${second}`
       },
+      onProcessBarChange(percent) {
+        this.$refs.audio.currentTime = this.currentSong.duration * percent
+        if (!this.playing) {
+          this.togglePlaying()
+        }
+      },
       _pad(num, n = 2) {
         let len = num.toString().length
         while (len < n) {
@@ -225,6 +234,9 @@
       disableCls() {
         return this.songReady ? '' : 'disable'
       },
+      percent() {
+        return this.currentTime / this.currentSong.duration
+      },
       ...mapGetters([
         'fullScreen',
         'playlist',
@@ -247,6 +259,7 @@
       }
     },
     components: {
+      ProgressBar
     }
   }
 </script>
